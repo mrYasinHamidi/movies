7a77a15251f419333dd86712032744e73fcf7ec2
Yasin <yasinhamidi50@gmail.com>
2023-10-02 10:10:28 -0700

    create gust session and initial route based on login status

---
+++ lib/auth/domain/session/session.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'session.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Session _$$_SessionFromJson(Map<String, dynamic> json) => _$_Session(
+      id: json['id'] as String,
+      isGuest: json['isGuest'] as bool,
+    );
+
+Map<String, dynamic> _$$_SessionToJson(_$_Session instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'isGuest': instance.isGuest,
+    };
